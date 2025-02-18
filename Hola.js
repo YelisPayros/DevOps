144485d2f7df6e65005477e22d09c7d17fa406cd
YelisPayros <nicolepayros@gmail.com>
2025-02-17 20:54:50 -0400

    PRUEBAS UNITARIAS

--- Hola.js
+++ Hola.js
@@ -2,5 +2,6 @@ function holaMundo() {
     return "Hola Mundo!";
   }
   
-  console.log(holaMundo()); // Imprime "Hola Mundo!" en la consola
+  console.log(holaMundo()); 
   
+  //Yelis Nicole Payero Rosa 2023-0930
